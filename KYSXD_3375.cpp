d423feb92a93935d266e5e2d2f2d3601097fe21f
KYSXD <dracojan_12@hotmail.com>
2015-09-26 22:09:02 -0500

    Soluciones 3323 y 3375

---
+++ KYSXD_3375.cpp
@@ -0,0 +1,32 @@
+//"""
+//#KYSXD - Problema 3375
+//"""
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n = 100;
+    int casos, cambios;
+
+    string A, B;
+    cin >> casos;
+    int contador[casos];
+
+    for(int i = 0; i < casos; i++){
+        cambios = 0;
+        cin >> A >> B;
+
+        for(int j = 0; j < min(A.length(), B.length()); j++){
+            if(A[j] != B[j]) {
+                cambios++;
+            }
+        }
+        cout << cambios << endl;
+    }
+    return 0;
+}
